bca2eb6130a86244dd4f819c160f66e363636507
ahmar7 <ahmarjabbar7@gmail.com>
2024-03-22 13:21:28 +0500

    uodate

--- BE/app.js
+++ BE/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", theOrigin);
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
